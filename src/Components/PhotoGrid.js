ff21b34624334c264b26585e595ce95d5fabc912
Rhys Maiden <maidenrhys@gmail.com>
2020-01-29 15:45:39 +0700

    Loader component

--- src/Components/PhotoGrid.js
+++ src/Components/PhotoGrid.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Photo from "./Photo.js";
+import Loader from "./Loader.js";
 
 const PhotoGrid = props => {
   return (
@@ -9,23 +10,16 @@ const PhotoGrid = props => {
       }
     >
       {props.loading ? (
-        <div class="lds-ring">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
+        <Loader />
       ) : (
-        <div></div>
+        <div className="row">
+          {props.photos.map(photo => (
+            <div className="col-3 col-sm-3 col-md-3 col-lg-2 photo-grid">
+              <Photo src={photo.node.thumbnail_src} />
+            </div>
+          ))}
+        </div>
       )}
-
-      <div className="row">
-        {props.photos.map(photo => (
-          <div className="col-3 col-sm-3 col-md-3 col-lg-2 photo-grid">
-            <Photo src={photo.node.thumbnail_src} />
-          </div>
-        ))}
-      </div>
     </div>
   );
 };
